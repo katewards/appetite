2376d4e0a6e37c382edf702057fbf68ae364f904
Kate Edwards <katewards@gmail.com>
2014-01-22 14:52:57 +0000

    set up the rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Appetite::Application.config.secret_key_base = 'c3d650f419cbf72db9d6e2e2fb5e95e20291a1352ee5c0b1ff3dcecba4878efc2212e8e7c0cf7017faf27bd8e9900e103f4794d02dd2ff740c1410f2c304c1c1'
